9a1bb8da7c2cfdec25504db249ec3cfd4012e69a
decoy <decoy4157@gmail.com>
2016-12-21 22:41:30 +0300

    added dump method to NewsEnt model and described get_news view

--- api/models.py
+++ api/models.py
@@ -6,4 +6,9 @@ class NewsEnt(models.Model):
 	title = models.CharField(max_length=200)		
 	body = models.TextField()
 	pub_date = models.DateTimeField()
-	
+
+	def dump(self):
+		return {"NewsItem": {'title': self.title,
+    						'body': self.body,
+    						'pub_date': self.pub_date}}
+   
--- api/views.py
+++ api/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.core import serializers
+from django.utils import timezone
+from django.utils.html import strip_tags, escape
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+import json
+from .models import NewsEnt
+
+def date_handler(obj):
+    return obj.isoformat() if hasattr(obj, 'isoformat') else obj
 
 def get_news(request):
-	return HttpResponse("You are on /news endpoint")
+	news = NewsEnt.objects.order_by('-pub_date')[:10]
+   	news = json.dumps([n.dump() for n in news], default=date_handler)
+   	return HttpResponse(news);
+        #serializers.serialize('json', news),
+        #content_type='application/json'
+        #news
+   		#)
 
 def post_new(request):
 	return HttpResponse("You are on /new endpoint")
