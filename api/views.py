289a132ca9f92c15fe3c9830ec27cb19ca4bb39c
decoy <decoy4157@gmail.com>
2016-12-21 21:52:00 +0300

    added request argument to views

--- api/views.py
+++ api/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def get_news():
+def get_news(request):
 	return HttpResponse("You are on /news endpoint")
 
-def post_new():
+def post_new(request):
 	return HttpResponse("You are on /new endpoint")
 
-def login():
+def login(request):
 	return HttpResponse("You are on /login endpoint")					
