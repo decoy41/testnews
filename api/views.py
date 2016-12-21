ce3bb163be6020b961213ae085c3d736c39bf4c3
decoy <decoy4157@gmail.com>
2016-12-21 23:13:47 +0300

    described post_new view

--- api/views.py
+++ api/views.py
@@ -4,7 +4,9 @@ from django.utils import timezone
 from django.utils.html import strip_tags, escape
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
 import json
+
 from .models import NewsEnt
 
 def date_handler(obj):
@@ -14,13 +16,21 @@ def get_news(request):
 	news = NewsEnt.objects.order_by('-pub_date')[:10]
    	news = json.dumps([n.dump() for n in news], default=date_handler)
    	return HttpResponse(news);
-        #serializers.serialize('json', news),
-        #content_type='application/json'
-        #news
-   		#)
 
 def post_new(request):
-	return HttpResponse("You are on /new endpoint")
+	if request.user.is_authenticated:
+		if request.method == 'POST':
+			json_data = json.loads(request.body)
+			new = NewsItem()
+			new.title = escape(strip_tags(json_data['title']))
+			new.body = escape(strip_tags(json_data['body']))
+			new.pub_date = timezone.now()
+			new.save()
+			return HttpResponse(status=200)
+		else:
+			return HttpResponse(status=403)	
+	else:
+		return redirect('./login')
 
 def login(request):
 	return HttpResponse("You are on /login endpoint")					
